e508b5cc005727aa279b503d2966f226d03d3418
MetaAnomie <MetaAnomie@users.noreply.github.com>
2023-05-08 15:03:14 +0900

    Added datetime stripping to RegexReplace

--- Encoders/RegexReplace/RegexReplace.py
+++ Encoders/RegexReplace/RegexReplace.py
@@ -4,19 +4,24 @@ import re
 
 class RegexReplace(Encoder):
 
-    def __init__(self, enable_defaults=True):
-        self._enableDefaultRules = enable_defaults
+    def __init__(self, enable_standard_rules=True, enable_datetime_rules=True):
         self._rules = {}
-        self.__loadDefaultRules()
+        self._enableStandardRules = enable_standard_rules
+        self._enableDatetimeRules = enable_datetime_rules
+        self.__loadDateTimeRules()
+        self.__loadStandardRules()
 
-# /mnt/hadoop/dfs/data/current/subdir57/blk_<:NUM:>
-# "((?<=[^A-Za-z0-9])|^) ((?=[^A-Za-z0-9])|$)": "<:PATH:>",
-
-    def __loadDefaultRules(self):
-        self._default_rules = {
+    def __loadDateTimeRules(self):
+        self._datetime_rules = {
             "((?<=[^A-Za-z0-9])|^)((0?[1-9]|1[012])[-/.](0?[1-9]|[12][0-9]|3[01])[-/.]([1-9]\d\d\d|[0-9]\d))((?=[^A-Za-z0-9])|$)": "<:DATE:>",
             "((?<=[^A-Za-z0-9])|^)((0?[1-9]|[12][0-9]|3[01])[-/.](0?[1-9]|1[012])[-/.]([1-9]\d\d\d|[0-9]\d))((?=[^A-Za-z0-9])|$)": "<:DATE:>",
             "((?<=[^A-Za-z0-9])|^)(([1-9]\d\d\d|[0-9]\d)[-/.](0?[1-9]|1[012])[-/.](0?[1-9]|[12][0-9]|3[01]))((?=[^A-Za-z0-9])|$)": "<:DATE:>",
+            "((?<=[^A-Za-z0-9])|^)([0-9]|0[0-9]|1[0-9]|2[0-3]):[0-5][0-9]:[0-5][0-9].[0-9]+((?=[^A-Za-z0-9])|$)": "<:TIME:>",
+            "((?<=[^A-Za-z0-9])|^)([0-9]|0[0-9]|1[0-9]|2[0-3]):[0-5][0-9](:[0-5][0-9])?((?=[^A-Za-z0-9])|$)": "<:TIME:>"
+        }
+
+    def __loadStandardRules(self):
+        self._standard_rules = {
             "((?<=[^A-Za-z0-9])|^)(\\d{1,3}\\.\\d{1,3}\\.\\d{1,3}\\.\\d{1,3})((?=[^A-Za-z0-9])|$)": "<:IP:>",
             "((?<=[^A-Za-z0-9])|^)(0x[a-f0-9A-F]+)((?=[^A-Za-z0-9])|$)": "<:HEX:>",
             "((?<=[^A-Za-z0-9])|^)([\\-\\+]?\\d+)((?=[^A-Za-z0-9])|$)": "<:NUM:>"
@@ -26,13 +31,19 @@ class RegexReplace(Encoder):
         if data is not None:
             for k, v in self._rules.items():
                 data = re.sub(k, v, data)
-        if self._enableDefaultRules:
-            for k, v in self._default_rules.items():
+        if self._enableDatetimeRules:
+            for k, v in self._datetime_rules.items():
+                data = re.sub(k, v, data)
+        if self._enableStandardRules:
+            for k, v in self._standard_rules.items():
                 data = re.sub(k, v, data)
         return data
 
-    def enable_default_rules(self, enable_defaults=True):
-        self._enableDefaultRules = enable_defaults
+    def enable_datetime_rules(self, enable_datetime_rules=True):
+        self._enableDatetimeRules = enable_datetime_rules
+
+    def enable_standard_rules(self, enable_standard_rules=True):
+        self._enableStandardRules = enable_standard_rules
 
     def add_replace_rule(self, rule=None, replace=None):
         if rule is not None:
