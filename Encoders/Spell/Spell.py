7649a65d5bc0f2dfeffe8a4ac3af3033d842de60
Scott Lupton <MetaAnomie@users.noreply.github.com>
2023-08-30 16:21:01 +0900

    Added Spell Encoder

---
+++ Encoders/Spell/Spell.py
@@ -0,0 +1,238 @@
+"""
+Description : This file implements the Spell algorithm for log parsing
+Author      : LogPAI team
+License     : MIT
+
+Revision History:
+
+2023.08.30 slupton: Revised implementation to parse streaming data as a pipeline component
+"""
+
+import re
+import os
+import pandas as pd
+import hashlib
+import string
+
+from Encoders.Encoder import Encoder
+
+
+class LCSObject:
+    """ Class object to store a log group with the same template
+    """
+
+    def __init__(self, logTemplate='', logIDL=[]):
+        self.logTemplate = logTemplate
+        self.logIDL = logIDL
+
+
+class Node:
+    """ A node in prefix tree data structure
+    """
+
+    def __init__(self, token='', templateNo=0):
+        self.logClust = None
+        self.token = token
+        self.templateNo = templateNo
+        self.childD = dict()
+
+
+class Spell(Encoder):
+    """ LogParser class
+
+    Attributes
+    ----------
+        tau : how much percentage of tokens matched to merge a log message
+    """
+
+    def __init__(self, tau=0.5, encode_to_indices=False):
+        self.tau = tau
+        self.cluster_id = 0
+        self.encode_to_indices = encode_to_indices
+        self.rootNode = Node()
+        self.logCluL = []
+
+
+    def LCS(self, seq1, seq2):
+        lengths = [[0 for j in range(len(seq2) + 1)] for i in range(len(seq1) + 1)]
+        # row 0 and column 0 are initialized to 0 already
+        for i in range(len(seq1)):
+            for j in range(len(seq2)):
+                if seq1[i] == seq2[j]:
+                    lengths[i + 1][j + 1] = lengths[i][j] + 1
+                else:
+                    lengths[i + 1][j + 1] = max(lengths[i + 1][j], lengths[i][j + 1])
+
+        # read the substring out from the matrix
+        result = []
+        lenOfSeq1, lenOfSeq2 = len(seq1), len(seq2)
+        while lenOfSeq1 != 0 and lenOfSeq2 != 0:
+            if lengths[lenOfSeq1][lenOfSeq2] == lengths[lenOfSeq1 - 1][lenOfSeq2]:
+                lenOfSeq1 -= 1
+            elif lengths[lenOfSeq1][lenOfSeq2] == lengths[lenOfSeq1][lenOfSeq2 - 1]:
+                lenOfSeq2 -= 1
+            else:
+                assert seq1[lenOfSeq1 - 1] == seq2[lenOfSeq2 - 1]
+                result.insert(0, seq1[lenOfSeq1 - 1])
+                lenOfSeq1 -= 1
+                lenOfSeq2 -= 1
+        return result
+
+    def SimpleLoopMatch(self, logClustL, seq):
+        for logClust in logClustL:
+            if float(len(logClust.logTemplate)) < 0.5 * len(seq):
+                continue
+            # Check the template is a subsequence of seq (we use set checking as a proxy here for speedup since
+            # incorrect-ordering bad cases rarely occur in logs)
+            token_set = set(seq)
+            if all(token in token_set or token == '<*>' for token in logClust.logTemplate):
+                return logClust
+        return None
+
+    def PrefixTreeMatch(self, parentn, seq, idx):
+        retLogClust = None
+        length = len(seq)
+        for i in range(idx, length):
+            if seq[i] in parentn.childD:
+                childn = parentn.childD[seq[i]]
+                if (childn.logClust is not None):
+                    constLM = [w for w in childn.logClust.logTemplate if w != '<*>']
+                    if float(len(constLM)) >= self.tau * length:
+                        return childn.logClust
+                else:
+                    return self.PrefixTreeMatch(childn, seq, i + 1)
+
+        return retLogClust
+
+    def LCSMatch(self, logClustL, seq):
+        retLogClust = None
+
+        maxLen = -1
+        maxlcs = []
+        maxClust = None
+        set_seq = set(seq)
+        size_seq = len(seq)
+        for logClust in logClustL:
+            set_template = set(logClust.logTemplate)
+            if len(set_seq & set_template) < 0.5 * size_seq:
+                continue
+            lcs = self.LCS(seq, logClust.logTemplate)
+            if len(lcs) > maxLen or (len(lcs) == maxLen and len(logClust.logTemplate) < len(maxClust.logTemplate)):
+                maxLen = len(lcs)
+                maxlcs = lcs
+                maxClust = logClust
+
+        # LCS should be large then tau * len(itself)
+        if float(maxLen) >= self.tau * size_seq:
+            retLogClust = maxClust
+
+        return retLogClust
+
+    def getTemplate(self, lcs, seq):
+        retVal = []
+        if not lcs:
+            return retVal
+
+        lcs = lcs[::-1]
+        i = 0
+        for token in seq:
+            i += 1
+            if token == lcs[-1]:
+                retVal.append(token)
+                lcs.pop()
+            else:
+                retVal.append('<*>')
+            if not lcs:
+                break
+        if i < len(seq):
+            retVal.append('<*>')
+        return retVal
+
+    def addSeqToPrefixTree(self, rootn, newCluster):
+        parentn = rootn
+        seq = newCluster.logTemplate
+        seq = [w for w in seq if w != '<*>']
+
+        for i in range(len(seq)):
+            tokenInSeq = seq[i]
+            # Match
+            if tokenInSeq in parentn.childD:
+                parentn.childD[tokenInSeq].templateNo += 1
+                # Do not Match
+            else:
+                parentn.childD[tokenInSeq] = Node(token=tokenInSeq, templateNo=1)
+            parentn = parentn.childD[tokenInSeq]
+
+        if parentn.logClust is None:
+            parentn.logClust = newCluster
+
+    def removeSeqFromPrefixTree(self, rootn, newCluster):
+        parentn = rootn
+        seq = newCluster.logTemplate
+        seq = [w for w in seq if w != '<*>']
+
+        for tokenInSeq in seq:
+            if tokenInSeq in parentn.childD:
+                matchedNode = parentn.childD[tokenInSeq]
+                if matchedNode.templateNo == 1:
+                    del parentn.childD[tokenInSeq]
+                    break
+                else:
+                    matchedNode.templateNo -= 1
+                    parentn = matchedNode
+
+    def printTree(self, node, dep):
+        pStr = ''
+        for i in range(dep):
+            pStr += '\t'
+
+        if node.token == '':
+            pStr += 'Root'
+        else:
+            pStr += node.token
+            if node.logClust is not None:
+                pStr += '-->' + ' '.join(node.logClust.logTemplate)
+        print(pStr + ' (' + str(node.templateNo) + ')')
+
+        for child in node.childD:
+            self.printTree(node.childD[child], dep + 1)
+
+    def encode(self, data):
+
+        retval = data
+
+        if data is not None and len(data) > 0:
+            #logmessageL = list(filter(lambda x: x != '', re.split(r'[\s=:,]', data)))
+            logmessageL = list(filter(lambda x: x != '', re.split(r'[\s=,]|(?<!\<(?!\>)):"', data)))
+            constLogMessL = [w for w in logmessageL if w != '<*>']
+
+            # Find an existing matched log cluster
+            matchCluster = self.PrefixTreeMatch(self.rootNode, constLogMessL, 0)
+
+            if matchCluster is None:
+                matchCluster = self.SimpleLoopMatch(self.logCluL, constLogMessL)
+
+                if matchCluster is None:
+                    matchCluster = self.LCSMatch(self.logCluL, logmessageL)
+
+                    # Create new cluster on no existing match
+                    if matchCluster is None:
+                        self.cluster_id += 1
+                        matchCluster = LCSObject(logTemplate=logmessageL, logIDL=self.cluster_id)
+                        self.logCluL.append(matchCluster)
+                        self.addSeqToPrefixTree(self.rootNode, matchCluster)
+
+                    # Add log message to existing cluster
+                    else:
+                        newTemplate = self.getTemplate(self.LCS(logmessageL, matchCluster.logTemplate),
+                                                       matchCluster.logTemplate)
+                        if ' '.join(newTemplate) != ' '.join(matchCluster.logTemplate):
+                            self.removeSeqFromPrefixTree(self.rootNode, matchCluster)
+                            matchCluster.logTemplate = newTemplate
+                            self.addSeqToPrefixTree(self.rootNode, matchCluster)
+
+            retval = ' '.join(matchCluster.logTemplate)
+            if self.encode_to_indices:
+                retval = matchCluster.logIDL
+
+            return retval
